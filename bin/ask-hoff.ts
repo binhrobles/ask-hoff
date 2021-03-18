a5f6bf35b6a3e1a1354cd34ebf9747fa962f6e48
Binh 🌱 <binhrobles@gmail.com>
2021-03-18 11:57:38 -0400

    Initial commit

---
+++ bin/ask-hoff.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { AskHoffStack } from '../lib/ask-hoff-stack';
+
+const app = new cdk.App();
+new AskHoffStack(app, 'AskHoffStack');
