9663eb8b89ac3e68fdd66f99be26a3dd1148631d
Binh 🌱 <binhrobles@gmail.com>
2021-03-31 13:13:15 -0400

    Add core hoff functionality

--- handlers/hoff/index.ts
+++ handlers/hoff/index.ts
@@ -6,15 +6,53 @@ import {
 } from "https://deno.land/x/lambda/mod.ts";
 
 const { MS_SECRET, GIPHY_TOKEN } = Deno.env.toObject();
+const MS_SECRET_UTF = atob(MS_SECRET);
 
-// deno-lint-ignore require-await
 export async function handler(
   event: APIGatewayProxyEvent,
   context: Context,
 ): Promise<APIGatewayProxyResult> {
-  return {
-    statusCode: 200,
-    headers: { "content-type": "text/html;charset=utf8" },
-    body: `Welcome to deno ${Deno.version.deno} 🦕`,
-  };
+  try {
+    let tone;
+    let answer;
+    const randomNum = Math.random();
+    if (randomNum < 0.3) {
+      tone = "positive";
+      answer = "The Hoff thinks yes!";
+    } else if (randomNum >= 0.3 || randomNum < 0.6) {
+      tone = "unsure";
+      answer = "Hoff's not sure.";
+    } else if (randomNum >= 0.6 || randomNum < 0.9) {
+      tone = "negative";
+      answer = "That's a no from the Hoff.";
+    } else if (randomNum >= 0.9) {
+      tone = "thoughtful";
+      answer = "The Hoff needs you to come back later, skater.";
+    }
+
+    const offset = Math.floor(Math.random() * 10);
+    const res = await fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_TOKEN}&q=${tone} hasselhoff&limit=1&offset=${offset}&rating=g&lang=en`,
+    );
+    const json = await res.json();
+    const imageInfo = json.data[0].images.original;
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        type: "AdaptiveCard",
+        text:
+          `${answer}<br/><img src="${imageInfo.url}" height="${imageInfo.height}" width="${imageInfo.width}"></img>`,
+      }),
+    };
+  } catch (err) {
+    console.error(err.message);
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        type: "message",
+        text: err.message,
+      }),
+    };
+  }
 }
